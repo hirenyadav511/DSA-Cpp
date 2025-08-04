4593ac3481596248cb9517c369b9c4c2bef90e30
hirenyadav511 <hirenyadav2004@gmail.com>
2025-08-04 14:25:02 +0530

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,141 @@
+#include <iostream> // Required for input/output
+using namespace std;
+
+int main()
+{
+    cout << "Namaste dunia :)" << endl;
+}
+
+// ----------  Datatypes  --------------
+
+// int main()
+// {
+
+// int a = 123;
+// cout<< a <<endl;
+
+// char b = 'h';
+// cout<< b <<endl;
+
+// float f = 1.2;
+// cout<< f <<endl;
+
+// bool bl = true;
+// cout<< bl <<endl;
+
+// double d = 1.234;
+// cout<< d <<endl;
+
+// int size = sizeof(a);
+// cout<< "size of a is : " << size <<endl;
+
+// int s = sizeof(d);
+// cout<< "size of d is : " << s <<endl;
+// }
+
+// ----------  TypeCasting -------------
+
+// int main()
+// {
+//     int a = 'a';
+//     cout << a << endl;
+
+//     char ch = 98;
+//     cout<< ch <<endl;
+// }
+
+// ------------  Operators  -------------
+
+// int main()
+// {
+//     int a = 2;
+//     int b = 3;
+
+//     bool first = (a==b);
+//     cout << first << endl;
+
+//     bool sec = (a>b);
+//     cout << sec << endl;
+
+//     bool third = (a<b);
+//     cout << third << endl;
+
+//     bool fourth = (a>=b);
+//     cout << fourth << endl;
+
+//     bool fifth = (a<=b);
+//     cout << fifth << endl;
+
+//     bool sixth = (a!=b);
+//     cout << sixth << endl;
+
+// }
+
+// -------- Bitwise Operators ----------
+
+// int main()
+// {
+//     int a = 4;
+//     int b = 6;
+
+//     cout << " a&b " << (a & b) << endl;
+//     cout << " a|b " << (a | b) << endl;
+//     cout << " ~a " << ~a << endl;
+//     cout << " a^b " << (a ^ b) << endl;
+
+// }
+
+//  ----- left & right shift operator -----
+
+// int main()
+// {
+//     cout << (17 >> 1) << endl;
+//     cout << (17 >> 2) << endl;
+//     cout << (19 << 1) << endl;
+//     cout << (21 << 2) << endl;
+// }
+
+//  ---- pre post increment decrement operator -----
+
+// int main()
+// {
+//     int i = 7;
+
+//     cout << ++i << endl;
+//     //8
+//     cout << i++ << endl;
+//     //8 , i = 9;
+//     cout << i-- << endl;
+//     //9 , i = 8;
+//     cout << --i << endl;
+//     //7
+// }
+
+// ------  Input value program   ----------------
+
+// int main()
+// {
+//     int a;
+//     int b;
+
+//     cin >> a >> b;
+
+//     cout << "value of a nd b is " << a << " " << b << endl;
+
+// }
+
+//  ----- continue keyword ------
+
+// int main()
+// {
+
+//     for (int i = 0; i < 5; i++)
+//     {
+//         cout << " hi " << endl;
+//         cout << " hello " << endl;
+//         continue;
+
+//         cout << " hello world! " << endl;
+//     }
+// }
+    
